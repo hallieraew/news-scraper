0c1c9d2864f81c6a28f0612aabb60be4e8404a3e
hallieraew <hallieweintraub@gmail.com>
2020-04-04 14:26:23 -0500

    initial try at scrape not successful

--- server.js
+++ server.js
@@ -20,6 +20,38 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
+
+app.get("/scrape", function(req, res) {
+
+    axios.get("https://www.nytimes.com/section/us").then(function(response) {
+
+        var $ = cheerio.load(response.data);
+
+        $("h2").each(function(i, element) {
+
+            var result = {};
+
+            result.headline = $(this).find("a").text();
+
+            result.link = $(this).children().find("a").attr("href");
+
+            result.summary = $(this).children().find("p").text();
+            console.log(result);
+
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        });
+
+        res.send("complete");
+    })
+})
